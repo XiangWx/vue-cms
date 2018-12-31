f5542ffa8bbbc4eeb8bffd983d9001350cce8a71
XiangWx <1013109374@qq.com>
2018-12-31 15:10:34 +0800

    header footer

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+//入口文件
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'mint-ui/lib/style.css'
+//引入Mint-ui组件
+import Mint from 'mint-ui'
+Vue.use(Mint)
+
+import { Header } from 'mint-ui'
+Vue.component(Header.name, Header)
+
+//导入Mui
+import './lib/mui/css/mui.css'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  render: c => c(App)
+
+  
 })
